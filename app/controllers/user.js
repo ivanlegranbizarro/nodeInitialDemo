1e33c2c292fa34e9426630f1a2ee4a550682dc94
Iván Legrán <68439095+ivanlegranbizarro@users.noreply.github.com>
2022-12-16 19:42:17 +0100

    fixing url

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -3,6 +3,6 @@ export const infoUser = ( req, res ) => {
   res.json( {
     nom: 'Iván Legrán',
     edat: 37,
-    url: req.originalUrl
+    url: `http://localhost:3000/${ req.originalUrl }`
   } );
 };

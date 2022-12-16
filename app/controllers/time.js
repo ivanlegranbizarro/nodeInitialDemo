3ce7329d57b85763dd7950472db036a1ee0e2e12
Iván Legrán <68439095+ivanlegranbizarro@users.noreply.github.com>
2022-12-16 19:33:45 +0100

    correccions

---
+++ app/controllers/time.js
@@ -0,0 +1,11 @@
+export const time = ( req, res ) => {
+  const { username } = req.body;
+  const date = new Date();
+
+  res.header( "Cache-control", "no-cache" );
+  res.header( "Access-Control-Allow-Origin", "*" );
+  res.header( "Authorization", "Basic legran 1234" + Buffer.from( username ).toString( "base64" ) );
+
+
+  res.json( { username, date } );
+};
--- app/routes/user.js
+++ app/routes/user.js
@@ -1,15 +1,9 @@
 import express from "express";
+import { infoUser } from "../controllers/user.js";
 
 const router = express.Router();
 
 /* GET enviant missatge sol·licitat per l'exercici. */
-router.get('/', (req, res) =>
-    res.json(
-        {
-            nom: 'Iván Legrán',
-            edat: 37,
-            url: 'http://localhost:3000/user'
-        }
-    ));
+router.get( '/', infoUser );
 
 export default router;

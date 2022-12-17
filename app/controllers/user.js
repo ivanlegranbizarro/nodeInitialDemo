9cca03a8a9fa2a2c6805a543f468ac10b95535be
Iván Legrán <68439095+ivanlegranbizarro@users.noreply.github.com>
2022-12-17 13:06:37 +0100

    fixing get full url

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -3,6 +3,6 @@ export const infoUser = ( req, res ) => {
   res.json( {
     nom: 'Iván Legrán',
     edat: 37,
-    url: `http://localhost:3000/${ req.originalUrl }`
+    url: `${ req.protocol }://${ req.get( 'host' ) }${ req.originalUrl }}`
   } );
 };

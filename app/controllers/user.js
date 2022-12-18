4b330bd8e7ad0ce7c13d60f92dbeb87deb877065
Iván Legrán <68439095+ivanlegranbizarro@users.noreply.github.com>
2022-12-18 09:00:37 +0100

    more typos

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -3,6 +3,6 @@ export const infoUser = ( req, res ) => {
   res.json( {
     nom: 'Iván Legrán',
     edat: 37,
-    url: `${ req.protocol }://${ req.get( 'host' ) }${ req.originalUrl }}`
+    url: `${ req.protocol }://${ req.get( 'host' ) }${ req.originalUrl }`
   } );
 };

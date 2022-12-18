c6e1f59e774a4a55656cf93022c4fa8ffc39a999
Iván Legrán <68439095+ivanlegranbizarro@users.noreply.github.com>
2022-12-18 11:38:12 +0100

    basic-auth

--- app/controllers/time.js
+++ app/controllers/time.js
@@ -1,11 +1,8 @@
 
+
 export const time = ( req, res ) => {
   const { username } = req.body;
   const date = new Date();
-
-  res.header( "Cache-control", "no-cache" );
-  res.header( "Authorization", "Basic legran 1234" + Buffer.from( username ).toString( "base64" ) );
-
 
   res.json( { username, date } );
 };
--- app/middlewares/timeAuth.js
+++ app/middlewares/timeAuth.js
@@ -1,21 +1,15 @@
 /*
 Afegeix un middleware a l'endpoint anterior que retorni un HTTP Status 401 - Unauthorized si la capçalera de la petició no conté autenticació bàsica (usuari/ària i contrasenya).*/
+import auth from "basic-auth";
 
-
-const basicAuth = (req, res, next) => {
-    const {username, password} = req.body;
-    if (!req.headers.authorization || req.headers.authorization.indexOf('Basic ') === -1) {
-        return res.status(401).json({message: 'Missing Authorization Header'});
+const basicAuth = ( req, res, next ) => {
+    const credentials = auth( req );
+    if ( !credentials || credentials.name !== "legran" || credentials.pass !== "1234" ) {
+        res.status( 401 ).json( { error: "Unauthorized" } );
     } else {
-        const base64Credentials = req.headers.authorization.split(' ')[1];
-        const credentials = Buffer.from(base64Credentials, 'base64').toString('ascii');
-        const [user, pass] = credentials.split(':');
-        if (user === username && pass === password) {
-            next();
-        } else {
-            return res.status(401).json({message: 'Invalid Authentication Credentials'});
-        }
+        next();
     }
 };
+
 
 export default basicAuth;

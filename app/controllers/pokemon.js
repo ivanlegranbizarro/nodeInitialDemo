f57055923ad800d4ce529f0e768a91f94de91aa5
Iván Legrán <ivanlegranbizarro@gmail.com>
2023-01-09 09:33:29 +0100

    validant id ruta Pokémon

--- app/controllers/pokemon.js
+++ app/controllers/pokemon.js
@@ -4,8 +4,23 @@ import fetch from "node-fetch";
 
 const getPokemon = async (req, res) => {
     const {id} = req.params;
+    if (isNaN(id)) {
+        res.status(400).json({
+            error: "id must be a number"
+        });
+        return;
+    }
+
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
     const pokemon = await response.json();
+    
+    if (pokemon.detail === "Not found.") {
+        res.status(404).json({
+            error: "Pokemon not found"
+        });
+        return;
+    }
+
     res.json({
         name: pokemon.name,
         height: pokemon.height,

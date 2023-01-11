b95c6182a154e746889b4488c4783be8fc1d2b19
Iván Legrán <68439095+ivanlegranbizarro@users.noreply.github.com>
2023-01-11 16:15:10 +0100

    Revert "updating"
    This reverts commit becb54961b65a231e2f5ca4531653d42ab28d41a.

--- app/routes/error.404.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-const router = express.Router();
-
-export const error404 = router.get( '*', ( req, res ) => {
-  res.status( 404 ).json( { error: '404 Not Found' } );
-} );
-
-
